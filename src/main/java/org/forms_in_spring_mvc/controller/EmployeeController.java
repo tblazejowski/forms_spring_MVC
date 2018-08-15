c5b9c38d830ddf8f2123c1a9ace35fb24d287bc9
tblazejowski <tomasz.blazejowski@gmail.com>
2018-08-15 18:51:10 +0200

    Initial commit

---
+++ src/main/java/org/forms_in_spring_mvc/controller/EmployeeController.java
@@ -0,0 +1,62 @@
+package org.forms_in_spring_mvc.controller;
+
+import org.forms_in_spring_mvc.domain.Employee;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.validation.Valid;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@Controller
+public class EmployeeController {
+
+    Map<Long, Employee> employeeMap = new HashMap<>();
+
+    @RequestMapping(value = "/employee", method = RequestMethod.GET)
+    public ModelAndView showForm() {
+        return new ModelAndView("employeeHome", "employee", new Employee());
+    }
+
+    @RequestMapping(value = "/employee/{Id}", method = RequestMethod.GET)
+    public @ResponseBody Employee getEmployeeById(@PathVariable final long Id) {
+        return employeeMap.get(Id);
+    }
+
+    /*@RequestMapping(value = "/getEmployee", method = RequestMethod.GET)
+    public @ResponseBody List<Employee> getEmployee() {
+        employeeList = employeeMap.values()
+                .stream()
+                .collect(Collectors.toList());
+        return employeeList;
+    }*/
+
+    @RequestMapping(value = "/getEmployee", method = RequestMethod.GET)
+    public String getEmployee (Model model){
+        List<Employee> employeeList = employeeMap.values()
+                .stream()
+                .collect(Collectors.toList());
+        model.addAttribute("employeeList", employeeList);
+        return "employeeAllView";
+    }
+
+    @RequestMapping(value = "/addEmployee", method = RequestMethod.POST)
+    public String submit(@Valid @ModelAttribute("employee") Employee employee, BindingResult result, ModelMap model) {
+        if (result.hasErrors()) {
+            return "error";
+        }
+        model.addAttribute("name", employee.getName());
+        model.addAttribute("contactNumber", employee.getContactNumber());
+        model.addAttribute("id", employee.getId());
+        employeeMap.put(employee.getId(), employee);
+        return "employeeView";
+    }
+
+}
---
+++ src/main/webapp/WEB-INF/employeeAllView.jsp
@@ -0,0 +1,51 @@
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%--
+  Created by IntelliJ IDEA.
+  User: Tommaso
+  Date: 15.08.2018
+  Time: 10:17
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<!DOCTYPE>
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <title>=== LIST OF ALL EMPLOYEE ===</title>
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
+          integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+</head>
+<body>
+<h3 align="center">Employee list:</h3>
+<div class="container">
+    <table class="table table-striped">
+        <thead class="thead-dark">
+        <tr>
+            <th scope="col">Id</th>
+            <th scope="col">Name</th>
+            <th scope="col">Phone Number</th>
+        </tr>
+        </thead>
+        <tbody>
+        <c:forEach items="${employeeList}" var="employee">
+            <tr>
+                <th scope="row">${employee.id}</th>
+                <td>${employee.name}</td>
+                <td>${employee.contactNumber}</td>
+            </tr>
+        </c:forEach>
+        </tbody>
+    </table>
+</div>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+        crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"
+        integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49"
+        crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"
+        integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy"
+        crossorigin="anonymous"></script>
+</body>
+</html>
